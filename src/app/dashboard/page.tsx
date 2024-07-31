de332500e098033118b1b9a128b9ae7c0ce66f29
Prateek462003 <prateeksaxena462003@gmail.com>
2024-08-01 01:22:22 +0530

    changed tailwind config

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,7 +7,7 @@ import TaskSection from "@/Components/TaskSection";
 export default function DashboardPage() {
   return (
     <div className="flex h-screen">
-      <div className="w-1/5  p-4">
+      <div className="w-1/5 p-4">
         <ProfileSection />
       </div>
       <div className="w-4/5 bg-gray-100 p-4">
